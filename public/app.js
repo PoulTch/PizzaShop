28b8f988717478c2bc4664e7e713e718f008ba81
PoulTch <pavelchar@gmail.com>
2025-01-20 13:32:04 +0300

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -17,4 +17,25 @@ function add_to_cart(id)
 	var x = window.localStorage.getItem(key);
 	x = x*1 + 1;
 	window.localStorage.setItem(key, x);
+
+	// вывод количества item'ов в корзине
+	alert('Items in your cart: ' + cart_get_number_of_items());
+}
+
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // get key
+		var value = window.localStorage.getItem(key); // get value, in Ruby is hh['key'] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1; // увеличиваем на единицу
+		}
+	}
+
+	return cnt;
 }
