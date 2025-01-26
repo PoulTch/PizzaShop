eef325fc991ec2ebf9fc73e453ed2a186af38043
PoulTch <pavelchar@gmail.com>
2025-01-26 11:53:35 +0300

    Replace id with product

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ set :database, {adapter: "sqlite3", database: "pizzashop.db"}
 
 class Product < ActiveRecord::Base
 end
-
+	
 get '/' do
 	@products = Product.all
 	erb :index			
@@ -20,26 +20,28 @@ end
 
 post '/cart' do
 	orders_input = params[:orders]
-	@orders = parse_orders_input orders_input
+	@items = parse_orders_input orders_input
+
+	@items.each do |item|
+		# id, cnt
+		item[0] = Product.find(item[0])
+	end
+
 	erb :cart
 end
 
 def parse_orders_input orders_input                                       
                                                                         
 	s1 = orders_input.split(/,/)                                     
-                                                                        
-	#puts s1.inspect                                                
+                                                                                                                        
 	arr = []                                                        
                                                                         
 	s1.each do |x|                                                  
-		s2 = x.split(/=/)                                       
-		#puts s2.inspect                                        
-		s3 = s2[0].split(/_/)                                   
-		#puts s3.inspect                                        
+		s2 = x.split(/=/)                                                                               
+		s3 = s2[0].split(/_/)                                                                           
                                                                         
 		id = s3[1]                                              
-		cnt = s2[1]                                             
-		#puts "Product id: #{key}, number of items: #{value}"   
+		cnt = s2[1]                                                
 		arr2 = [id, cnt]                                        
                                                                         
 		arr.push arr2
